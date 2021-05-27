792cd3404135cdcfde5b5d4c6bfb2358584dac92
george murphy <georgeolivermurphy@hotmail.com>
2021-05-27 11:36:55 +0100

    uncompleted case

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // Event Listeners
-todoButton.addEventListener('click', addTodo);
-todoList.addEventListener('click', deleteCheck);
-
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 // Functions
 function addTodo(event) {
@@ -43,7 +44,11 @@ function deleteCheck (e) {
   // DELETE TODO
   if(item.classList[0] === 'trash-btn') {
     const todo = item.parentElement;
+    // ANIMATION
     todo.classList.add("fall");
+    todo.addEventListener('transitionend', function() {
+      todo.remove
+    });
   }
 
   // CHECK MARK
@@ -51,4 +56,30 @@ function deleteCheck (e) {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterTodo (e) {
+  // grab all the todos
+  const todos = todoList.childNodes;
+  todos.forEach(function(todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if(!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
